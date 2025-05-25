df822d30c046bb891c6159504048d640a8009b4e
jimin990 <wlals3236@naver.com>
2025-05-25 20:34:57 +0900

    86

---
+++ coding-study/Num_30404.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+	int N, K;
+
+	std::cin >> N >> K;
+	std::vector<int> vec;
+
+	if (N == 1) {
+	
+		std::cout << "1";
+		return 0;
+	}
+
+	for (int i=0; i < N; i++) {
+		int X;
+		std::cin >> X;
+		vec.push_back(X);
+	}
+
+
+	int count = 1;
+	int K_tmp = K;
+
+	for (int i = 0; i < N-1; i++) {
+		K_tmp -= vec[i + 1] - vec[i];
+
+		if (K_tmp < 0) {
+			K_tmp = K;
+			count++;
+		}
+		
+	}
+
+	std::cout << count;
+
+	return 0;
+}
