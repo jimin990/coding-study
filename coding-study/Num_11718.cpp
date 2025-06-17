67a7d841c096c493417fad49006aff6ce7effaf4
KIMJIMIN <wlals3236@naver.com>
2025-06-17 18:06:36 +0900

    110

---
+++ coding-study/Num_11718.cpp
@@ -0,0 +1,21 @@
+//
+//  Num_11718.cpp
+//  coding-test
+//
+//  Created by 김지민 on 6/17/25.
+//
+
+#include <iostream>
+#include <string>
+
+int main(){
+    
+    std::string str;
+    
+    while(std::getline(std::cin,str)){
+        std::cout << str << "\n";
+        
+    }
+    
+    return 0;
+}
