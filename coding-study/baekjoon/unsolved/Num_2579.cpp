d9cbb8827ae385e2a8399b95904bb2a93b1b2015
jimin990 <wlals3236@naver.com>
2025-07-17 13:38:05 +0900

    solve to 2579.cpp

--- coding-study/baekjoon/unsolved/Num_2579.cpp
+++ coding-study/baekjoon/unsolved/Num_2579.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main(void){
-    
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
     int N;
     std::cin >> N;
-    
-    int* arr = new int[N];
-    int* arr1 = new int[N];
 
-    for(int i =0;i<N;i++){
+    int* arr = new int[N+1];
+
+    std::vector<std::vector<int>> arr1(N+1,std::vector<int>(3,0));
+
+    arr[0] = 0;
+
+    for(int i =1;i<=N;i++){
         std::cin >> arr[i];
     }
 
-    arr1[0] = arr[0];
-    arr1[1] = arr[1]+arr[0];
-    arr1[2] = arr[2] + arr[1] + arr[0];
+    arr1[1][0] = 0;
+    
+    for(int i =1;i<3;i++){
+        
+        arr1[1][i] = arr[1];
+    }
+    
+    for(int i =2;i<=N;i++){
 
-    for(int i =3;i<N;i++){
-        //arr1[i] = std::max(arr[i] + arr1[i-2],arr );
+        arr1[i][0] = std::max(arr1[i-1][1],arr1[i-1][2]);
+        arr1[i][1] = arr[i] + arr1[i-1][0];
+        arr1[i][2] = arr[i] + arr1[i-1][1];
     }
 
-    for(int i =0;i<N;i++){
-        std::cout << arr1[i] << " ";
-    }
+    std::cout << std::max(arr1[N][1],arr1[N][2]);
 
-    std::sort(arr1,arr1+N);
-
-    std::cout << arr1[N-1];
     return 0;
 }
