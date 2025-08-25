6452a4fb93444b1bf6fdb6b4d1d3bd4468ee2361
jimin990 <wlals3236@naver.com>
2025-08-25 09:07:14 +0900

    177

---
+++ coding-study/baekjoon/bronze/Num_7287.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void){
+    
+    std::cout << "355\nYour_ICPC_Team_Name";
+    return 0;
+}
---
+++ coding-study/baekjoon/unsolved/Num_2420.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main(void){
+
+    int N,M;
+    std::cin >> N >> M;
+    std::cout << std::abs(N-M);
+    return 0;
+}
