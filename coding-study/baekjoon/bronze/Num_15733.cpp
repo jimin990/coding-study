12d00ee343295d91ffe4edb0872611ddc22e8038
jimin990 <wlals3236@naver.com>
2025-08-28 10:22:45 +0900

    180

---
+++ coding-study/baekjoon/bronze/Num_15733.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void){
+
+    std::cout << "I'm Sexy";
+    return 0;
+}
---
+++ coding-study/baekjoon/unsolved/Num_21939.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <set>
+#include <utility>
+#include <string>
+#include <map>
+
+int main(void){
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int N;
+
+    std::cin >> N;
+
+    std::map<int,int> problem_level;
+
+    std::set<std::pair<int,int>> level_problem;
+
+    for(int i =0;i<N;i++){
+
+        int P,L;
+
+        std::cin >> P >> L;
+
+        problem_level.insert({P,L});
+
+        level_problem.insert({L,P});
+    }
+
+    int M;
+
+    std::cin >> M;
+
+    for(int i =0;i<M;i++){
+
+        std::string commend;
+
+        std::cin >> commend;
+
+        if(commend == "recommend"){
+            int x;
+
+            std::cin >> x;
+
+            if(x == 1){
+                std::cout << (--level_problem.end()) -> second << "\n";
+            }else{
+                std::cout << (level_problem.begin()) -> second << "\n";
+            }
+        }else if(commend == "add"){
+
+            int P,L;
+
+            std::cin >> P >> L;
+
+            problem_level.insert({P,L});
+            level_problem.insert({L,P});
+
+        }else{
+            int P;
+
+            std::cin >> P;
+
+            int level = problem_level[P];
+            problem_level.erase(P);
+            level_problem.erase({level,P});
+
+        }
+    }
+
+    
+
+    return 0;
+}
