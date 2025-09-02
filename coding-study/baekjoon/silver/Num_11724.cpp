44c703fd182edaf1de933ae7e03cdd167fc68e5c
jimin990 <wlals3236@naver.com>
2025-09-02 22:54:17 +0900

    Num_11724_progress2

--- coding-study/baekjoon/silver/Num_11724.cpp
+++ coding-study/baekjoon/silver/Num_11724.cpp
@@ -3,14 +3,13 @@
 
 int N, M;
 
-std::vector<std::vector<bool>> graph(N,std::vector<bool>(N));
-
-void recrusion(int j){
+void recrusion(int j,std::vector<std::vector<bool>> &graph){
 
     for(int i=0;i<N;i++){
         if(graph[j][i]){
             graph[j][i] = false;
-            recrusion(i);
+            graph[i][j] = false;
+            recrusion(i,graph);
         }
     }
 }
@@ -19,23 +18,33 @@ int main(void){
 
     std::cin >> N >> M;
 
+    std::vector<std::vector<bool>> graph(N,std::vector<bool>(N));
+
     for(int i =0;i<M;i++){
         int u,v;
 
         std::cin >> u >> v;
 
         graph[u - 1][v - 1] = true;
+        graph[v - 1][u - 1] = true;
     }
+
+    int component = 0;
 
     for(int i =0;i<N;i++){
         for(int j =0;j<N;j++){
             if(graph[i][j]){
+
+                component++;
+
                 graph[i][j] = false;
-                recrusion(j);
+                graph[j][i] = false;
+                recrusion(j,graph);
             }
         }
     }
 
+    std::cout << component;
 
     return 0;
 }
