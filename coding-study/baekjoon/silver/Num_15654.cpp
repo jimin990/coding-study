c6ce1e4fd73c06e883b2e1d3a237281cd2845232
jimin990 <wlals3236@naver.com>
2025-09-10 11:20:01 +0900

    Num_15654

---
+++ coding-study/baekjoon/silver/Num_15654.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int N,M;
+
+int arr[10];
+bool isused[10];
+
+void func(int num,std::vector<int> &num_set){
+
+    if(num == M){
+        
+        for(int i =0;i<M;i++){
+            std::cout << arr[i] << " ";
+        }
+
+        std::cout << "\n";
+        
+        return;
+    }
+
+    for(int i =0;i<N;i++){
+        if(!isused[i]){
+            arr[num] = num_set[i];
+            
+            isused[i] = true;
+            func(num+1,num_set);
+            isused[i] = false;
+        }
+    }
+}
+
+int main(void){
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    std::cin >> N >> M;
+
+    std::vector<int> num_set(N);
+
+    for(int i=0;i<N;i++){
+
+        std::cin >> num_set[i];
+    }
+
+    std::sort(num_set.begin(),num_set.end());
+
+    func(0,num_set);
+
+    return 0;
+}
