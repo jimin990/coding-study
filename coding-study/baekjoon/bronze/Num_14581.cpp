cb9268be38c15920c342f112b4b5d0d460f89b34
jimin990 <wlals3236@naver.com>
2025-09-26 20:10:08 +0900

    209

---
+++ coding-study/baekjoon/bronze/Num_14581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+
+    std::string str;
+    std::cin >> str;
+
+    std::cout << ":fan::fan::fan:" << "\n";
+    std::cout << ":fan::" << str << "::fan:" << "\n";
+    std::cout << ":fan::fan::fan:";
+
+    return 0;
+}
+
