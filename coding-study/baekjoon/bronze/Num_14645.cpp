cc14df4525b5cff4b7880daf8e36a7c1eddc9c7c
jimin990 <wlals3236@naver.com>
2025-09-29 19:33:09 +0900

    212

---
+++ coding-study/baekjoon/bronze/Num_14645.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void){
+
+    int N,K;
+    
+    std::cin >> N >> K;
+
+    for(int i =0;i<N;i++){
+
+        int m,l;
+
+        std::cin >> m >> l;
+    }
+
+    std::cout <<"비와이";
+
+    return 0;
+}
