b72dadec47d4c20b678402c7f081976cbdc2f7b8
jimin990 <wlals3236@naver.com>
2025-10-14 09:34:15 +0900

    226

---
+++ coding-study/baekjoon/bronze/Num_15963.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void){
+
+    long A,B;
+
+    std::cin >> A >> B;
+
+    if(A == B){
+        std::cout << 1;
+    }else{
+        std::cout << 0;
+    }
+
+    return 0;
+}
