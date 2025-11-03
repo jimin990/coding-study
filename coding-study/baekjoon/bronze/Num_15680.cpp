48b9053fba137fd962375f1de2dcc2996db40146
jimin990 <wlals3236@naver.com>
2025-11-03 13:53:16 +0900

    247

---
+++ coding-study/baekjoon/bronze/Num_15680.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void){
+
+    int N;
+
+    std::cin >> N;
+
+    if(N == 0) std::cout << "YONSEI";
+    else std::cout << "Leading the Way to the Future";
+
+    return 0;
+}
