cebf250970323b26d654ce2a0e4daa72875d462a
jimin990 <wlals3236@naver.com>
2025-11-08 19:15:00 +0900

    252

---
+++ coding-study/baekjoon/bronze/Num_32384.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+    int main(void){
+
+        int N;
+
+        std::cin>> N;
+
+        for(int i=0;i<N;i++){
+            std::cout<< "LoveisKoreaUniversity" << " ";
+        }
+        return 0;
+    }
