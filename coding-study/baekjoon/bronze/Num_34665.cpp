4542a244fe3befe0be90fe7e3ad76f4494aa9bb7
jimin990 <wlals3236@naver.com>
2025-11-26 09:39:44 +0900

    270

---
+++ coding-study/baekjoon/bronze/Num_34665.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+
+    std::string str1, str2;
+
+    std::cin >> str1 >> str2;
+
+    std::cout << ((str1 == str2) ? 0 : 1550);
+
+    return 0;
+}
