bb9b18e35c6da02bd9812f0342aa8a5fadd5ac1c
jimin990 <wlals3236@naver.com>
2025-12-03 13:42:58 +0900

    277

---
+++ coding-study/baekjoon/bronze/Num_34073.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(void){
+
+    int N;
+
+    std::cin >> N;
+
+    for(int i =0;i<N;i++){
+
+        std::string str;
+
+        std::cin >> str;
+
+        std::cout << str << "DORO "; 
+
+    }
+
+    return 0;
+}
