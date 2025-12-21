c0f5b8d51f4c3237f11bc0491840a54cd02ead9a
jimin990 <wlals3236@naver.com>
2025-12-21 19:46:41 +0900

    295

---
+++ coding-study/baekjoon/bronze/Num_32326.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+
+    int red, green, blue;
+
+    std::cin >> red >> green >> blue;
+
+    std::cout << (red * 3) + (green * 4) + (blue * 5);
+
+    return 0;
+}
