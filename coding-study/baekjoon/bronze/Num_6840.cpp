643634f045174b699e4e80ef0cb73575761f1525
jimin990 <wlals3236@naver.com>
2025-12-22 22:18:39 +0900

    296

---
+++ coding-study/baekjoon/bronze/Num_6840.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void){
+
+    std::vector<int> arr(3,0);
+    
+    std::cin >> arr[0] >> arr[1] >> arr[2];
+
+    std::sort(arr.begin(), arr.end());
+
+    std::cout << arr[1];
+
+    return 0;
+}
