91a027e5524a0d08feeaad1c7c41021eb559a441
jimin990 <wlals3236@naver.com>
2025-12-24 21:57:27 +0900

    298

---
+++ coding-study/baekjoon/bronze/Num_9316.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void){
+
+    int N;
+
+    std::cin >> N;
+
+    for(int i =1;i<=N;i++){
+
+        std::cout << "Hello World, Judge " << i << "!\n";
+    }
+
+    return 0;
+}
