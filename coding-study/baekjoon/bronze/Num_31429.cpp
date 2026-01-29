6019261f2371491dea1dfe8840b8191f2c3113bc
jimin990 <wlals3236@naver.com>
2026-01-29 15:42:26 +0900

    334

---
+++ coding-study/baekjoon/bronze/Num_31429.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main(void)
+{
+
+    int N;
+
+    std::cin >> N;
+    
+    if(N == 1)
+    {
+        std::cout << "12 1600";
+    }else if(N == 2)
+    {
+        std::cout << "11 894";
+    }else if(N == 3)
+    {
+        std::cout << "11 1327";
+    }else if(N == 4)
+    {
+        std::cout << "10 1311";
+    }else if(N == 5)
+    {
+        std::cout << "9 1004";
+    }else if(N == 6)
+    {
+        std::cout << "9 1178";
+    }else if(N == 7)
+    {
+        std::cout << "9 1357";
+    }else if(N == 8)
+    {
+        std::cout << "8 837";
+    }else if(N == 9)
+    {
+        std::cout << "7 1055";
+    }else if(N == 10)
+    {
+        std::cout << "6 556";
+    }else
+    {
+        std::cout << "6 773";
+    }
+}
