065834426d6aaf795f95d97c8c48e32c6c1696bb
jimin990 <wlals3236@naver.com>
2026-03-18 20:18:24 +0900

    382

---
+++ coding-study/baekjoon/bronze/Num_32154.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main(void)
+{
+    int N;
+
+    std::cin >> N;
+
+    if(N == 1)
+    {
+        std::cout << 11 << "\n" << "A B C D E F G H J L M";
+    }else if(N == 2)
+    {
+        std::cout << 9 << "\n" << "A C E F G H I L M";
+    }else if(N == 3)
+    {
+        std::cout << 9 << "\n" << "A C E F G H I L M";
+    }else if(N == 4)
+    {
+        std::cout << 9 << "\n" << "A B C E F G H L M";
+    }else if(N == 5)
+    {
+        std::cout << 8 << "\n" << "A C E F G H L M";
+    }else if(N == 6)
+    {
+        std::cout << 8 << "\n" << "A C E F G H L M";
+    }else if(N == 7)
+    {
+        std::cout << 8 << "\n" << "A C E F G H L M";
+    }else if(N == 8)
+    {
+        std::cout << 8 << "\n" << "A C E F G H L M";
+    }else if(N == 9)
+    {
+        std::cout << 8 << "\n" << "A C E F G H L M";
+    }else if(N == 10)
+    {
+        std::cout << 8 << "\n" << "A B C F G H L M";
+    }
+
+    return 0;
+}
