d60098c76ad93c53f1e0f888f2ea9ef2f76a1dbe
jimin990 <wlals3236@naver.com>
2026-04-06 20:52:19 +0900

    401

--- coding-study/baekjoon/bronze/Num_11943.cpp
+++ coding-study/baekjoon/bronze/Num_11943.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
-int main(void){
+int main(void)
+{
+    int A, B, C, D;
 
-    std::cout << "고려대학교";
+    std::cin >> A >> B >> C >> D;
 
+    if((A + D) <= (B + C))
+    {
+        std::cout << (A + D);
+    }else
+    {
+        std::cout << (B + C);
+    }
+    
     return 0;
 }
